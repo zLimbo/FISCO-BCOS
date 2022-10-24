94ab9722b2612eab21552bf92507aec0d177a84e
z_limbo <z_limbo@foxmail.com>
2022-10-24 15:46:20 +0800

    statistics

--- libconsensus/raft/RaftEngine.cpp
+++ libconsensus/raft/RaftEngine.cpp
@@ -1676,7 +1676,7 @@ bool RaftEngine::reachBlockIntervalTime()
     auto parentTime = m_lastBlockTime;
 
     // return nowTime - parentTime >= g_BCOSConfig.c_intervalBlockTime;
-    return nowTime - parentTime >= 500;
+    return nowTime - parentTime >= 1000;
 }
 
 const std::string RaftEngine::consensusStatus()
